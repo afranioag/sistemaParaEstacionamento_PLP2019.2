bd84e2d1ce7b9aedb5bf5adc84fa3e8900971b02
Danilo César Ribeiro Garcia de Medeiros <danilocrgm@gmail.com>
2019-10-14 19:28:10 -0300

    Adicionei função para salvar e recuperar um cliente

--- Haskell/Cliente.hs
+++ Haskell/Cliente.hs
@@ -3,25 +3,57 @@ module Cliente(
     clienteStatus,
     clienteVeiculo,
     clienteVaga,
-    clienteVeiculo,
-    clienteMostraCliente
+    clienteMostraCliente,
+    salvaCliente,
+    recuperaCliente
 )
 
 where
 
-data Cliente = Cliente{cpf::String, veiculo::String, status::String, valor::Float, vaga::Int}
+import Data.List.Split
+
+data Cliente = Cliente{cpf:: String, placa::String, veiculo::String, status::String, valor::Float, vaga::Int}
+
+clienteCpf::Cliente->String
+clienteCpf (Cliente cpf _ _ _ _ _) = cpf
 
 clienteStatus::Cliente->String
-clienteStatus (Cliente _ _ status _ _) = status
+clienteStatus (Cliente _ _ _ status _ _) = status
 
 clienteVeiculo::Cliente->String
-clienteVeiculo (Cliente _ veiculo _ _ _) =  veiculo
+clienteVeiculo (Cliente _ _ veiculo _ _ _) =  veiculo
 
 clienteVaga::Cliente->Int
-clienteVaga (Cliente _ _ _ _ vaga) = vaga
+clienteVaga (Cliente _ _ _ _ _ vaga) = vaga
 
-clienteAtualizaValor::Cliente->Cliente
-clienteAtualizaValor (Cliente a b c d e) novo = (Cliente a b c d novo e)
+clienteAtualizaValor::Cliente->Float->Cliente
+clienteAtualizaValor (Cliente a b c d e f) novo = (Cliente a b c d novo f)
 
 clienteMostraCliente::Cliente->String
-clienteMostraCliente (Cliente a b c d e) = "Cliente: " ++ a ++ "\nVeiculo: " ++ b ++ "\nStatus: " ++ c ++ "\nValor: " ++ (show d) ++ "\nVaga: " ++ e
+clienteMostraCliente (Cliente a b c d e f) = "\nCPF: " ++ a ++ "\nPlaca: " ++ b ++ "\nVeiculo: " ++ c ++ "\nStatus: " ++ d ++ "\nValor: " ++ (show e) ++ "\nVaga: " ++ (show f)
+
+formataCliente:: Cliente->String
+formataCliente (Cliente a b c d e f) = a ++ "-" ++ b ++ "-" ++ c ++ "-" ++ d ++ "-" ++ (show e) ++ "-" ++ (show f)++ "\n"
+
+isCliente:: [String]->String->String
+isCliente [] key = ""
+isCliente (x:xs) key =
+    if (splitOn "-" x !! 0) == key then x
+    else isCliente xs key
+
+-- incompleta
+recuperaCliente:: String->String-> String
+recuperaCliente clientes cpf = do
+    let encontrado = isCliente (splitOn "\n" clientes) cpf
+    if encontrado == "" then "" else encontrado
+
+lerArquivo::IO String
+lerArquivo = readFile "clientes.txt"
+-- incompleta
+salvaCliente::Cliente->IO()
+salvaCliente cliente  = do
+    clientes <- lerArquivo
+    let flag = recuperaCliente clientes (clienteCpf cliente)
+    if flag /= "" then putStrLn $ "Cliente já existe" else appendFile "clientes.txt" $ formataCliente cliente
+--buscaCliente::String->Cliente
+--
