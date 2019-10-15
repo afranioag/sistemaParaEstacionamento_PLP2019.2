6ee640bdd6fb07e3153c271cb02872641a1bf666
João Carlos F <joao.bernardo@ccc.ufcg.edu.br>
2019-10-15 18:03:25 -0300

    getDataAtual implementado

--- Haskell/data.hs
+++ Haskell/data.hs
@@ -1,8 +1,14 @@
 import Data.Time.Clock
 import Data.Time.LocalTime
+import Data.Time.Calendar
 
 getHoraAtual = do
     now <- getCurrentTime
     timezone <- getCurrentTimeZone
     let (TimeOfDay hour minute second) = localTimeOfDay $ utcToLocalTime timezone now
     putStrLn $ show hour ++ ":" ++ show minute
+
+getDataAtual = do
+    now <- getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay now
+    putStrLn $ show day ++ "/" ++ show month ++ "/" ++ show year
