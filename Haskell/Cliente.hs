4a32c10f4b8a6d1fe0ab18bae61c9f88577b8445
Danilo César Ribeiro Garcia de Medeiros <danilocrgm@gmail.com>
2019-10-15 21:55:40 -0300

    Adicionado remoçao de cliente

--- Haskell/Cliente.hs
+++ Haskell/Cliente.hs
@@ -1,16 +1,21 @@
 module Cliente(
     Cliente(..),
+    geraCliente,
     clienteStatus,
     clienteVeiculo,
     clienteVaga,
     clienteMostraCliente,
+    clienteAtualizaValor,
     salvaCliente,
-    recuperaCliente
+    recuperaCliente,
+    lerArquivo,
+    removeCliente
 )
 
 where
 
 import Data.List.Split
+import System.Directory
 
 data Cliente = Cliente{cpf:: String, placa::String, veiculo::String, status::String, valor::Float, vaga::Int}
 
@@ -32,6 +37,13 @@ clienteAtualizaValor (Cliente a b c d e f) novo = (Cliente a b c d novo f)
 clienteMostraCliente::Cliente->String
 clienteMostraCliente (Cliente a b c d e f) = "\nCPF: " ++ a ++ "\nPlaca: " ++ b ++ "\nVeiculo: " ++ c ++ "\nStatus: " ++ d ++ "\nValor: " ++ (show e) ++ "\nVaga: " ++ (show f)
 
+geraCliente:: String->Cliente
+geraCliente cliente  =
+    if cliente /= "" then(Cliente (dados !! 0) (dados !! 1) (dados !! 2) (dados !! 3) (read (dados !! 4)::Float) (read(dados !! 5)::Int))
+    else (Cliente "" "" "" "" 0 0)
+    where
+        dados = splitOn "-" cliente
+
 formataCliente:: Cliente->String
 formataCliente (Cliente a b c d e f) = a ++ "-" ++ b ++ "-" ++ c ++ "-" ++ d ++ "-" ++ (show e) ++ "-" ++ (show f)++ "\n"
 
@@ -41,19 +53,34 @@ isCliente (x:xs) key =
     if (splitOn "-" x !! 0) == key then x
     else isCliente xs key
 
--- incompleta
 recuperaCliente:: String->String-> String
 recuperaCliente clientes cpf = do
-    let encontrado = isCliente (splitOn "\n" clientes) cpf
-    if encontrado == "" then "" else encontrado
+    encontrado
+    where
+        encontrado = isCliente (splitOn "\n" clientes) cpf
 
 lerArquivo::IO String
 lerArquivo = readFile "clientes.txt"
--- incompleta
+
 salvaCliente::Cliente->IO()
 salvaCliente cliente  = do
     clientes <- lerArquivo
     let flag = recuperaCliente clientes (clienteCpf cliente)
-    if flag /= "" then putStrLn $ "Cliente já existe" else appendFile "clientes.txt" $ formataCliente cliente
---buscaCliente::String->Cliente
---
+    if flag /= "" then putStrLn $ "Cliente já existe"
+    else do
+        appendFile "clientes.txt" $ formataCliente cliente
+        putStrLn $ "Cliente cadastrado."
+
+removeClienteAux::String->[String]->[String]
+removeClienteAux cpf [] = []
+removeClienteAux cpf (x:xs) =
+    if (splitOn "-" x !! 0) == cpf then removeClienteAux cpf xs
+    else x:removeClienteAux cpf xs
+
+removeCliente::String->IO()
+removeCliente cpf = do
+    clientes <- lerArquivo
+    let removeCliente = removeClienteAux cpf (splitOn "\n" clientes)
+    removeFile "clientes.txt"
+    writeFile "clientes.txt" $ init(unlines removeCliente)
+
