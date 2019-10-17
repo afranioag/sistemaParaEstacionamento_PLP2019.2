8b5a4c5b5ec2bce7ed64c67910fd7837f099c41a
Danilo César Ribeiro Garcia de Medeiros <danilocrgm@gmail.com>
2019-10-16 23:02:32 -0300

    Implementado reserva

--- Haskell/menu.hs
+++ Haskell/menu.hs
@@ -12,56 +12,79 @@ pegaStatus = do
     else if opt == 2 then return "normal"
         else pegaStatus
 
-cadastro:: IO()
-cadastro = do
-    putStrLn $ "Cadastre seu veículo"
+-- Falta implementar o checkin caso o cliente ja exista.
+checkInCliente:: IO()
+checkInCliente = do
+    putStrLn $ "Bem vindo ao checkIn do estacionamento."
     putStrLn $ "CPF: "
+    clientes <- lerArquivo
     cpf <- getLine
     putStrLn $ "Placa: "
     placa <- getLine
     putStrLn $ "Veiculo: "
     veiculo <- getLine
-    putStrLn $ "Status: "
-    status <- pegaStatus
-    -- função não implementada
-    vagas <- lerEstacionamento "vagas.txt"
-    let vaga = alocaVaga (splitOn "\n" vagas)
-    salvaCliente (Cliente cpf placa veiculo status 0.0 vaga)
-    atualizaVaga (show vaga) "vagas.txt"
+    hora <- getHoraAtual
+    minuto <- getMinutoAtual
+    let cliente = geraCliente (recuperaCliente clientes cpf)
+    if (clienteCpf cliente == "") then do
+        putStrLn $ "CheckIn novo cliente."
+        vagas <- lerEstacionamento ("dados/vagas.txt")
+        let vaga = alocaVaga (splitOn "\n" vagas)
+        salvaCliente (Cliente cpf placa veiculo "normal" 0.0 vaga hora minuto)
+        atualizaVaga "True" (show vaga) ("dados/vagas.txt")
+        putStrLn $ "CheckIn feito!"
+    else do
+        putStrLn $ "CheckIn cliente antigo."
+        let temp = clienteVaga
+        if temp != -1 then
 
 
-{-reserva:: IO()-}
---reserva = do
-    --putStrLn $ "Reserva de vaga para clientes VIP"
-    --putStrLn $ "Digite seu cpf: "
-    --cpf <- getLine
-    --if verificaCpf cpf then fazReserva cpf
-    {-else putStrLn $ "Cliente não existe."-}
 
-pagar:: IO()
-pagar = do
+reserva:: IO()
+reserva = do
+    putStrLn $ "Reserva de vaga para clientes VIP"
+    putStrLn $ "Digite seu cpf: "
+    cpf <- getLine
+    clientes <- lerArquivo
+    let cliente = geraCliente (recuperaCliente clientes cpf)
+    if (clienteCpf cliente) == "" then putStrLn $ "Cliente não existe."
+    else do
+        let status = clienteStatus cliente
+        if status == "normal" then putStrLn $ "Cliente não é VIP. Impossível fazer reserva"
+        else do
+            vagas <- lerEstacionamento "dados/vagas.txt"
+            let vaga = alocaVaga (splitOn "\n" vagas)
+            atualizaVaga "True" (show vaga) "dados/vagas.txt"
+            atualizaCliente cpf vaga
+
+checkOut:: IO()
+checkOut = do
     putStrLn $ "Pagar saida do estacionamento"
     putStrLn $ "Digite seu cpf: "
     cpf <- getLine
     clientes <- lerArquivo
     let cliente = geraCliente (recuperaCliente clientes cpf)
-    let valor = 10
-    let clienteAtualizado = clienteAtualizaValor cliente valor
-    removeCliente cpf
-    putStrLn $ clienteMostraCliente clienteAtualizado
+    if (clienteCpf cliente) == "" then putStrLn $ "Cliente não existe."
+    else do
+        valor <- calculaValor cliente
+        let clienteAtualizado = clienteAtualizaValor cliente valor
+        atualizaCliente cpf (-1)
+        atualizaVaga "False" (show clienteVaga) "dados/vagas.txt"
+        putStrLn $ clienteMostraCliente clienteAtualizado
 
 meuMenu:: Bool->IO()
 meuMenu True = return ()
 meuMenu saida = do
-    putStrLn $ "1 - Cadastrar"
+    putStrLn $ "1 - CheckIn"
     putStrLn $ "2 - Reservar"
-    putStrLn $ "3 - Pagar"
-    putStrLn $ "4 - Sair"
+    putStrLn $ "3 - CheckOut"
+    putStrLn $ "4 - CheckIn Funcionario"
+    putStrLn $ "5 - Sair"
     putStr $ "opt>> "
     opt <- getLine
     if opt == "1"
         then do
-            cadastro
+            checkInCliente
             meuMenu False
     else
         if opt == "2"
@@ -71,14 +94,20 @@ meuMenu saida = do
         else
             if opt == "3"
                 then do
-                    pagar
+                    checkOut
                     meuMenu False
             else
                 if opt == "4"
-                    then meuMenu True
-                else do
-                    putStrLn $ "Opção invalida"
-                    meuMenu False
+                    then do
+                        --checkInFuncionario
+                        meuMenu False
+                else
+                    if opt == "5"
+                        then meuMenu True
+                    else do
+                        putStrLn $ "Opção invalida"
+                        meuMenu False
 
 inicializaEstacionamento = do
+    criaEstacionamento
     meuMenu False
